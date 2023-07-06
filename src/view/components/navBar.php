ef2dcd1ceee5be40134eeace2be6979eff9d7f5e
Emmanuel Fajardo <emmanueljesuscadena@gmail.com>
2023-07-06 16:56:05 -0500

    cambios sencillos

--- src/view/components/navBar.php
+++ src/view/components/navBar.php
@@ -1,6 +1,6 @@
 <ul class="navBar">
     <li class="point"><a href="index.php"><img src="src\img\nombreLogo.jpg" alt="nombreLogo" class="nombreLogo"></a></li>
-    <li class="point"><a href="index.php">Inicio</a> <i class="fas fa-home"></i> </li>
+    <li><a class="point" href="index.php">Inicio</a> <i class="fas fa-home"></i> </li>
     <li class="products">Productos
         <i class="fas fa-shopping-bag"></i>
         <ul class="list__products">
@@ -9,8 +9,8 @@
             <li><a href="">Cuidado Facial</a></li>
         </ul>
     </li>
-    <li class="point"><a href="">Combos </a><i class="fas fa-box"></i></li>
-    <li class="point"><a href="">Caja Misteriosa</a> <i class="fas fa-archive"></i></li>
-    <li class="point"><a href="#sobreNosotros">Sobre Nosotros</a> <i class="fas fa-users"></i></li>
+    <li><a class="point" href="">Combos </a><i class="fas fa-box"></i></li>
+    <li><a class="point" href="src/view/cajasSorpresas.php">Cajas Sorpresa</a> <i class="fas fa-archive"></i></li>
+    <li><a class="point" href="#sobreNosotros">Sobre Nosotros</a> <i class="fas fa-users"></i></li>
     <li class="contentCart point"><i class="fas fa-shopping-cart fa-2x"></i></li>
 </ul>
